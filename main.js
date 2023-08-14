2e021a3ff5c4eec54f95b200b56b1149894c0767
mads <madssunshine10@gmail.com>
2023-08-14 19:39:11 +1000

    new stuff

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var day = 0;
+function next_day_function() {
+    day+=1;
+    pastday = day-1
+    console.log(day);
+
+    try {
+        document.getElementById("days-"+day).classList = "day-active";
+    } catch (error) {
+        console.log("Last day")
+        document.getElementById("next-day").style.display = 'none';
+    }
+    
+    try {
+       document.getElementById("days-"+pastday).classList = "day-past";
+    } catch (typeerror) {
+        console.log("first day");
+    }
+}
